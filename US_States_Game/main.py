ea0a09c0353fa5f48d3fd9df1d12893edaba5aca
Gobisan Ananthanadarajan <88422076+gobisan14@users.noreply.github.com>
2022-07-08 12:00:48 +0530

    Update Arcade

--- US_States_Game/main.py
+++ US_States_Game/main.py
@@ -16,10 +16,7 @@ while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50", prompt="Enter a state name? ").title()
 
     if answer_state == "Exit":
-        missing_states = []
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states = [state for state in all_states if state not in guessed_states]
         df = pandas.DataFrame(missing_states)
         df.to_csv("states_to_learn.csv")
         break
